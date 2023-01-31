19d16c54467dc880c849885fdd2a5a44cdab48f1
AnaBeatrizGalvao <bea.gvillas@gmail.com>
2023-01-30 22:39:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+//1 - crie uma função que exiba uma mensagem no console
+// function minhaFuncao(comida){
+//     return comida
+// }
+
+// const mensagem = ("batata")
+// console.log(mensagem)
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+// function exibirMeuNome(nome){
+//     return nome
+//   }
+//   console.log(exibirMeuNome("Ana"))
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+// const dados2 = function(nome, idade, musical){
+//     console.log(nome)
+//     console.log(idade)
+//     console.log(musical)
+// }
+
+// dados2("Ana","17","Todos")
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+// let DVD = ["Homem-Aranha no Aranhaverso", "Amiga da Minha Mulher"]
+
+// console.log(`${DVD}`)
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+// function soma(n1,n2){
+//     return n1*n2
+// }
+
+// console.log(soma(5,3))
+
+//6 - crie uma função que  verifique se uma  variável é true ou false
+// let n1 = 1
+// let n2 = 10
+
+// if(n1 < n2){
+//     console.log(true)
+// } else{
+//     console.log(false)
+// }
